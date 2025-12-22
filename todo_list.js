65195e131999ad1e2f62222f47a3745427f21136
one080310 <joyewon10345@gmail.com>
2025-12-22 14:22:00 +0900

    Delete todo_list.js

--- todo_list.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('input').forEach(cb => {
-  cb.addEventListener('change', () => {
-    cb.parentElement.style.textDecoration =
-      cb.checked ? 'line-through' : '';
-  });
-});
