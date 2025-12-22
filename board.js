0e2ce7ca6142a4666ca2c85d01d5672faad0740b
one080310 <joyewon10345@gmail.com>
2025-12-22 14:17:38 +0900

    Update board.js

--- board.js
+++ board.js
@@ -1,29 +1,8 @@
-let currentClass = 1;
+document.querySelectorAll('input[type="checkbox"]').forEach(box => {
+  const id = box.dataset.id;
+  box.checked = localStorage.getItem(id) === 'true';
 
-function selectClass(num) {
-  currentClass = num;
-
-  document.querySelectorAll('.class-btn')
-    .forEach(b => b.classList.remove('active'));
-
-  event.target.classList.add('active');
-}
-
-function createPost() {
-  const title = postTitle.value;
-  const content = postContent.value;
-
-  if (!title || !content) {
-    alert("입력해");
-    return;
-  }
-
-  const div = document.createElement('div');
-  div.className = 'post';
-  div.innerHTML = `<h4>${title}</h4><p>${content}</p>`;
-
-  postsList.prepend(div);
-
-  postTitle.value = "";
-  postContent.value = "";
-}
+  box.addEventListener('change', () => {
+    localStorage.setItem(id, box.checked);
+  });
+});
