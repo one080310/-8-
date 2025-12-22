82852ada02eb3ac5f747009b969c6bef441f7627
one080310 <joyewon10345@gmail.com>
2025-12-22 13:48:00 +0900

    Update study_check.js

--- study_check.js
+++ study_check.js
@@ -1,1 +1,10 @@
+document.querySelectorAll('#studyTable input[type="checkbox"]').forEach(cb => {
+  cb.addEventListener('change', () => {
+    if (cb.checked) {
+      cb.closest('tr').style.backgroundColor = '#e6fffa';
+    } else {
+      cb.closest('tr').style.backgroundColor = '';
+    }
+  });
+});
 
