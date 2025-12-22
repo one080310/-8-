5407b2aaf82d37e3bc66cce9e17ede836e064ea4
one080310 <joyewon10345@gmail.com>
2025-12-22 13:47:31 +0900

    Update todo_list.js

--- todo_list.js
+++ todo_list.js
@@ -1,1 +1,6 @@
 
+document.querySelectorAll('.todo-item input[type="checkbox"]').forEach(cb => {
+  cb.addEventListener('change', () => {
+    cb.parentElement.classList.toggle('completed', cb.checked);
+  });
+});
