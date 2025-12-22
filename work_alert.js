7eb1e70dc8a5ec771331a328210213d1fd94806e
one080310 <joyewon10345@gmail.com>
2025-12-22 14:22:23 +0900

    Delete work_alert.js

--- work_alert.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.homework input').forEach(cb => {
-  cb.addEventListener('change', () => {
-    cb.nextElementSibling.style.textDecoration =
-      cb.checked ? 'line-through' : '';
-  });
-});
